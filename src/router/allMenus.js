4524717c56571f95776ef6608d372c8e5fa55458
milaozhi <hljdxjiang@163.com>
2020-07-16 16:25:19 +0800

    增加部分菜单。后期补充功能

--- src/router/allMenus.js
+++ src/router/allMenus.js
@@ -37,6 +37,11 @@ export default {
           component: "Subjects",
         },
         {
+          key: "/index/OrderType",
+          title: "学科管理",
+          component: "OrderType",
+        },
+        {
           key: "/index/CourseManager",
           title: "课程管理",
           component: "CourseManager",
@@ -61,19 +66,78 @@ export default {
       ],
     },
     {
-      title: "销售机会",
+      title: "销售线索",
       icon: "scan",
       key: "salesleads",
       children: [
         {
           key: "/index/mysales",
-          title: "销售机会",
+          title: "我的客户",
           component: "MySales",
         },
         {
-          key: "/index/mysales",
-          title: "课程管理",
-          component: "CourseManager",
+          key: "/index/publicpond",
+          title: "公海数据",
+          component: "PublicPond",
+        },
+        {
+          key: "/index/OrderQuery",
+          title: "订单查询",
+          component: "OrderQuery",
+        },
+        {
+          key: "/index/allocate",
+          title: "数据分配",
+          component: "Allocate",
+        },
+        {
+          key: "/index/datasetting",
+          title: "数据规则设置",
+          component: "DataSetting",
+        },
+      ],
+    },
+    {
+      title: "OA办公",
+      icon: "scan",
+      key: "office",
+      children: [
+        {
+          key: "/index/attendance",
+          title: "考勤信息",
+          component: "Attendance",
+        },
+        {
+          key: "/index/expense",
+          title: "报销申请",
+          component: "Expense",
+        },
+        {
+          key: "/index/offwork",
+          title: "请假申请",
+          component: "Offwork",
+        },
+      ],
+    },
+    {
+      title: "系统设定",
+      icon: "scan",
+      key: "system",
+      children: [
+        {
+          key: "/index/parameter",
+          title: "系统参数",
+          component: "Parameter",
+        },
+        {
+          key: "/index/process",
+          title: "流程编辑",
+          component: "Process",
+        },
+        {
+          key: "/index/notice",
+          title: "公告管理",
+          component: "Notice",
         },
       ],
     },
